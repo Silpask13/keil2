e6b5c365f8f3039a115990034b4ee5337faea05e
Silpa Nair <silpask13@gmail.com>
2024-07-19 16:12:56 +0530

    Add files via upload

---
+++ bee1.c
@@ -0,0 +1,26 @@
+#include <LPC214X.h>
+void delay(unsigned int count);
+int main() {
+int i,j;
+for(i=0;i<8;i++)
+{
+	IODIR0 |= (1<< i); 
+}
+for(i=0;i<8;i++){
+	for(j=0;j<=i;j++){
+		IOSET0 |= (1 << j);
+		delay(200000);
+		}
+		for(j=0;j<8;j++)
+		{
+		IOCLR0 |= (1 << j);
+		}
+		delay(200000);
+		}
+return 0;
+}
+
+void delay(unsigned int count){
+	unsigned int i;
+	for(i=0;i<count;i++);
+}
